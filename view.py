43fa334cc1b91b453b2860c7faab3d1000b78d5f
andre <kompromisss111@gmail.com>
2024-10-11 19:36:49 +0300

    map view

--- view.py
+++ view.py
@@ -6,15 +6,25 @@ class View:
     def __init__(self):
         plt.ion()
         self.m_fig, self.m_axs = plt.subplots(1, 1, figsize=(5, 5))
+        self.m_axs.set_facecolor("black")
+
+        self.m_track = {}
+        self.m_track_store = 10
 
     def update(self, _info):
+        self.m_axs.cla()
+
         wanted = _info["wantedList"]
         map_size = _info["mapSize"]
         enemies = _info["enemies"]
         anomalies = _info["anomalies"]
         bounties = _info["bounties"]
 
-        # me = _info[""]
+        transports = _info["transports"]
+
+        if not len(self.m_track.keys()):
+            for transport in transports:
+                self.m_track[transport["id"]] = {"x": [transport["x"]], "y": [transport["y"]]}
 
         self.m_axs.set_ylim(0, map_size["y"])
         self.m_axs.set_xlim(0, map_size["x"])
@@ -31,6 +41,19 @@ class View:
         for bounty in bounties:
             self.m_axs.scatter(bounty["x"], bounty["y"], s=bounty["radius"], c="yellow")
 
+        for transport in transports:
+            self.m_axs.scatter(transport["x"], transport["y"], alpha=transport["health"] / 100, c="green")
+            self.m_track[transport["id"]]["x"].append(transport["x"])
+            self.m_track[transport["id"]]["y"].append(transport["y"])
+
+            self.m_axs.plot(self.m_track[transport["id"]]["x"], self.m_track[transport["id"]]["y"], color="green",
+                            linestyle='-')
+
+            if len(self.m_track[transport["id"]]["x"]) > self.m_track_store:
+                self.m_track[transport["id"]]["x"] = self.m_track[transport["id"]]["x"][1:]
+                self.m_track[transport["id"]]["y"] = self.m_track[transport["id"]]["y"][1:]
+
+
         plt.pause(0.1)
         ...
 
@@ -43,4 +66,5 @@ if __name__ == '__main__':
 
     v = View()
     v.update(test_data)
-    v.update()
+    v.update(test_data)
+    plt.show()
