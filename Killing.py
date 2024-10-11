749a9cccbc3070c17390075ee1ce2ae9cfecbdf2
DmBzerol <dima_borisov_1989@inbox.ru>
2024-10-11 20:36:08 +0300

    sum

--- Killing.py
+++ Killing.py
@@ -1,10 +1,18 @@
 class Killing:
 
-    def try_to_kill(self, transports):
+    def try_to_kill(self, transports, enemies):
+
+        # проверка на то что наши ковры не в зоне атаки
+        # если получаем больше, то стреляем и по своим
+        # Проверка у кого меньше хп
 
         commands_to_transport = []
         for transport in transports:
             transport_x = transport['x']
             transport_y = transport['y']
+
+            
+
+
 
 
